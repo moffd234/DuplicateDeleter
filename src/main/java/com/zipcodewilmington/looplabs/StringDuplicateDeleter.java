bb52c118db2ed3bd0e7ebf2457721c7419b78d9c
moffd234 <moffd234@gmail.com>
2024-04-13 20:51:47 -0400

    Completed testRemoveDups for Strings

--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -11,11 +11,41 @@ public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
 
     @Override
     public String[] removeDuplicates(int maxNumberOfDuplications) {
-        return new String[0];
+        String[] output = new String[0];
+        // Iterate over the initial array
+        for(int i = 0; i < array.length; i++){
+            int count = 0;
+            // Count how many times the integer at array[i] occurs
+            for(int j = 0; j < array.length; j++){
+                if(array[i].equals(array[j])){
+                    count ++;
+                }
+            }
+            if(count < maxNumberOfDuplications){
+                output = insertedString(output, array[i], output.length);
+            }
+        }
+        return output;
     }
 
     @Override
     public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
         return new String[0];
+    }
+    public String[] insertedString (String[] array, String insert, int index) {
+        // Create a new array with one additional element
+        String[] newArray = new String[array.length + 1];
+
+        // Copy elements from the old array to the new array, inserting the new element at the specified index
+        for (int i = 0; i < newArray.length; i++) {
+            if (i < index) {
+                newArray[i] = array[i];
+            } else if (i == index) {
+                newArray[i] = insert;
+            } else {
+                newArray[i] = array[i - 1];
+            }
+        }
+        return newArray;
     }
 }
